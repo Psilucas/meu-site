205c9a2b412a9353783e236c5ac5f3ccd96e60f2
Psilucas <lucasefamilia707@gmail.com>
2023-07-21 21:19:19 -0300

    javascript

---
+++ javascript/variaveis.js
@@ -0,0 +1,14 @@
+console.log(nome)
+//usando var ou nada
+// identificador
+//   atribuicao
+// valor
+var nome = "Lucas";
+sobrenome = "Marques";
+console.log(nome)
+
+//não permite a execução antes da criação (ERRO)
+let nome2 = "Senac"
+console.log(nome2)
+
+const pi=3.141923
